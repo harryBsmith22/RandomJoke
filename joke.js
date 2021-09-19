c2f2565362e1c8eac375d2f8ce41ec8bf923f606
Harry Smith <harrysmac@Harrys-MacBook-Air.local>
2021-09-19 13:23:29 +0100

    create joke function and add abse string variables

---
+++ joke.js
@@ -0,0 +1,8 @@
+const randomJoke = () => {
+    const knockString = 'Knock Knock';
+    const whoString = "Who's there ?";
+    const thereString = " who ?";
+
+    const randomString = ['Donald', 'Chicken', 'Jake', 'Doctor'];
+    const randomPunch = ['Duck', 'Finger', 'Peralta', 'doMeALittle' ];
+}
