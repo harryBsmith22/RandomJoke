36d497c6d23933efb5053d24ac771e6b60f1e968
Harry Smith <harrysmac@Harrys-MacBook-Air.local>
2021-09-19 13:25:26 +0100

    output joke strings in logs with timeout

--- joke.js
+++ joke.js
@@ -9,4 +9,22 @@ const randomJoke = () => {
     let chosenString = randomString[Math.floor(Math.random()*randomString.length)];
     let chosenPunch = randomPunch[Math.floor(Math.random()*randomPunch.length)];
 
+    setTimeout(() => {
+        console.log(knockString);
+    },0);
+
+    setTimeout(() => {
+        console.log(whoString);
+    },2000)
+
+    setTimeout(() => {
+        console.log(chosenString)
+    },4000)
+
+    setTimeout(() => {
+        console.log(chosenString + thereString);
+    },6000)
+    setTimeout(() => {
+        console.log(chosenString + ' ' + chosenPunch);
+    },8000)
 }
